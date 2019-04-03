3472e6836c869305c9860abf8857457a092f7dc0
diogen_work <diogendota.work@gmail.com>
2019-04-03 18:47:46 +0300

    firts content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="container">
+    <div class="header">
+      Post trucks
+    </div>
+    <div class="top-table">
+      <div class="top-table__controls-wrapper">
+        <div class="top-table__control">play</div>
+        <div class="top-table__control">new</div>
+        <div class="top-table__control">delete</div>
+        <div class="top-table__control">repost</div>
+      </div>
+      <div class="top-table__header">
+        <div
+          class="top-table__header-item"
+          v-for="(value,key) in top_table.fields"
+          :key="key+'_'+value"
+        >{{value}}</div>
+      </div>
+    </div>
+    <modal></modal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
+import modal from './components/Modal'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    modal
+  },
+  data: function () {
+    return {
+      top_table: {
+        fields: [
+          'Call Control',
+          'Read',
+          'Sound / Auto Call',
+          'Truck',
+          'Origin',
+          'Destination',
+          'Pickup',
+          'DH-O',
+          'DH-D',
+          'F/P',
+          'Length',
+          'Weight',
+          'Trip',
+          'Alarm',
+          'Actions'
+        ]
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  .top-table {
+    display: flex;
+    flex-direction: column;
+
+    &__controls-wrapper {
+      display: flex;
+    }
+
+    &__control {
+      display: flex;
+      justify-content: center;
+      margin: 0 10px;
+      width: 150px;
+      background-color: black;
+      color: white;
+    }
+
+    &__header {
+      display: flex;
+      margin-top: 20px;
+    }
+
+    &__header-item {
+      flex-basis: calc(100% / 15);
+      flex-grow: 1;
+      padding: 8px 0;
+      background-color: black;
+      color: white;
+      text-align: center;
+      border-left: 1px solid #dfdfdf;
+      border-right: 1px solid #dfdfdf;
+    }
+  }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="modal">
+        <div class="modal-close">&times;</div>
+        <div class="modal-inner">
+            <div class="modal-body">
+                <input type="text" name="call_control" placeholder="Call Control" class="modal__input">
+                <input type="text" name="read" placeholder="Read" class="modal__input">
+                <input type="text" name="sound" placeholder="Sound / Auto Call" class="modal__input">
+                <input type="text" name="truck" placeholder="Truck" class="modal__input">
+                <input type="text" name="origin" placeholder="Origin" class="modal__input">
+                <input type="text" name="destination" placeholder="Destination" class="modal__input">
+                <input type="text" name="pickup" placeholder="Pickup" class="modal__input">
+                <input type="text" name="dh_o" placeholder="DH-O" class="modal__input">
+                <input type="text" name="dh_d" placeholder="DH-D" class="modal__input">
+                <input type="text" name="f_p" placeholder="F/P" class="modal__input">
+                <input type="text" name="length" placeholder="Length" class="modal__input">
+                <input type="text" name="weight" placeholder="Weight" class="modal__input">
+                <input type="text" name="trip" placeholder="Trip" class="modal__input">
+                <input type="text" name="alarm" placeholder="Alarm" class="modal__input">
+                <input type="text" name="actions" placeholder="Actions" class="modal__input">
+                <button>Submit</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .modal {
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        background-color: rgba(0,0,0,0.25);
+        z-index: 999;
+        justify-content: center;
+        align-items: center;
+
+        &-inner {
+            background-color: white;
+            padding: 40px 80px;
+            max-width: 992px;
+        }
+
+        &-body {
+            display: flex;
+            flex-direction: column;
+        }
+
+        &__input {
+            margin: 8px 0;
+            font-size: 18px;
+        }
+    }
+</style>
